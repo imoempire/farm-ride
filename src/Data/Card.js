e225da35a8012ec8700f1b2afc03ee493e4775b6
imoempire <mrimoempire@gmail.com>
2022-03-09 15:40:05 +0000

    add styling files and Card component for Visa

---
+++ src/Data/Card.js
@@ -0,0 +1,12 @@
+export const card = [
+   {type: 'Bank Card',balance: '539', cardNumber: '234-345-4940', id: 2, image: require('../../assets/colors.jpg')},
+   {type: 'Bank Card',balance: '2439', cardNumber: '346-637-5633', id: 3, image: require('../../assets/color.jpg')},
+   {type: 'Bank Card',balance: '139', cardNumber: '875-738-8373', id: 4, image: require('../../assets/colors.jpg')},
+   {type: 'Bank Card',balance: '290', cardNumber: '673-378-3783', id: 5, image: require('../../assets/color.jpg')},
+]
+
+export const network = [
+   { net: 'Item 1', id: '1', value:"mtn" },
+   { net: 'Item 2', id: '2', value:"AitelTigo" },
+   { net: 'Item 3', id: '3', value:"Vodafone" },
+ ];
