ec2724ec0f0dfb00cdf2448ff3f6712916ab6ec0
imoempire <mrimoempire@gmail.com>
2022-03-09 15:32:28 +0000

    add Top bar component for other componenets

---
+++ src/Component/TopBar/TopBar.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TopBar = ({Title, background, TitleColor }) => {
+  return (
+    <View style={{width: '90%', marginHorizontal: 50, borderRadius: 10}}>
+      <View style={[styles.bar, {backgroundColor: background,}]}>
+         <Text style={{color: TitleColor, fontWeight: 'bold', fontSize: 20}}>{Title}</Text>
+      </View>
+    </View>
+  )
+}
+
+export default TopBar
+
+const styles = StyleSheet.create({
+   bar: {
+      alignItems: "center",
+      padding: 10,
+      borderRadius: 10
+   }
+})
