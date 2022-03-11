7d87949133da6433909be83b5626306530a3c4cb
imoempire <mrimoempire@gmail.com>
2022-03-11 13:09:38 +0000

    change history page route to extra page route

--- src/Navigation/BottomTabs.js
+++ src/Navigation/BottomTabs.js
@@ -1,12 +1,12 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import HistoryPage from "../Screens/HistoryPage";
+import Extra from "../Screens/Extra";
 import Wallet from "../Screens/Wallet";
 import Home from "../Screens/Home";
 import Settings from "../Screens/Settings";
 // Icons
-import { Feather, FontAwesome, Entypo } from "@expo/vector-icons";
+import { Feather, FontAwesome, Entypo,AntDesign } from "@expo/vector-icons";
 import Svg, { Path } from "react-native-svg";
 
 const Tab = createBottomTabNavigator();
@@ -28,8 +28,8 @@ const BottomTabs = () => {
                     <View style={{ flex: 1, backgroundColor: '#27E20C' }}></View>
                     <Svg
                         width={75}
-                        height={61}
-                        viewBox="0 0 75 61"
+                        height={60}
+                        viewBox="0 0 75 60"
                     >
                         <Path
                             d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
@@ -146,15 +146,15 @@ const BottomTabs = () => {
         }}
       />
       <Tab.Screen
-        name="History"
-        component={HistoryPage}
+        name="Extra"
+        component={Extra}
         options={{
           tabBarLabel: ({ focused, color, size }) => (
             (color = "white"),
             (size = 13),
             (
               <Text style={{ color: focused ? color : "black", fontSize: size, top: focused ? 20 : 0 }}>
-                History
+                Extra
               </Text>
             )
           ),
@@ -162,7 +162,7 @@ const BottomTabs = () => {
             (color="white"),
             <View>
               <View>
-                <FontAwesome style={{top: focused ? 10 : 0 }} name="history" size={24} color={focused ? color : 'black'} />
+              <AntDesign style={{top: focused ? 10 : 0 }} color={focused ? color : 'black'} name="appstore1" size={24} />              
               </View>
             </View>
           ),
